ebbe99955c122f61a367119792aa392edee9b27e
Saad ur Rehman <125679304+saadrehman10@users.noreply.github.com>
2024-02-02 21:38:42 +0500

    test 02

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -12,7 +12,7 @@ import { useSelector } from 'react-redux';
 export default function Navbar() {
 
     const cartData = useSelector(state => state)
-    console.log(cartData)   
+    console.log(cartData)
 
 
     return (
