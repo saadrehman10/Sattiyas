6a8a80cbf85cf073ed9004fc876cea996145167e
Saad ur Rehman <125679304+saadrehman10@users.noreply.github.com>
2024-02-05 22:22:15 +0500

    Updated code

--- app/testComponent/page.js
+++ app/testComponent/page.js
@@ -3,7 +3,7 @@ import Home_slider from "@/components/home_slider/Home_slider";
 export default function() {
     return(
         <>
-        <Home_slider></Home_slider>
+         <Home_slider></Home_slider>
         </>
     )
 }
--- components/home_slider/Home_slider.js
+++ components/home_slider/Home_slider.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import hero2 from  '/public/hero2.jpg';
 import animated_text from '/public/animated_text.png';
+import React from "react";
 import Slider from "react-slick";
 
 export default function Home_slider () {
@@ -12,111 +13,111 @@ export default function Home_slider () {
         slidesToShow: 1,
         slidesToScroll: 1
       };
+      
     return(
         <>
-        {/* <Slider {...settings}>
-
-
-
-        </Slider> */}
             <section className="container-fluid">
-                <div className="cs_slider position-relative">
-                <div className="cs_slider_container" data-autoplay="1" data-loop="1" data-speed="700" data-center="0"
-                    data-slides-per-view="1" data-fade-slide="1">
-                    <div className="cs_slider_wrapper">
-                    <div className="slick_slide_in">
-                        <div className="cs_hero cs_style_2 position-relative">
-                        <div className="cs_hero_left">
-                            <div className="cs_hero_text">
-                            <h4 className="cs_hero_title_mini cs_fs_28">Up to 50% Off</h4>
-                            <h1 className="cs_hero_title cs_fs_67 cs_bold">Elegant men collection in summer season</h1>
-                            <a href="shop.html" className="cs_btn cs_style_1 cs_fs_16 cs_medium">See Collection</a>
+            <Slider {...settings}>
+                    <div className="cs_slider position-relative">
+                        <div className="cs_slider_container" data-autoplay="1" data-loop="1" data-speed="700" data-center="0"
+                            data-slides-per-view="1" data-fade-slide="1">
+                            <div className="cs_slider_wrapper">
+                            <div className="slick_slide_in">
+                                <div className="cs_hero cs_style_2 position-relative">
+                                <div className="cs_hero_left">
+                                    <div className="cs_hero_text">
+                                    <h4 className="cs_hero_title_mini cs_fs_28">Up to 50% Off</h4>
+                                    <h1 className="cs_hero_title cs_fs_67 cs_bold">Elegant men collection in summer season</h1>
+                                    <a href="shop.html" className="cs_btn cs_style_1 cs_fs_16 cs_medium">See Collection</a>
+                                    </div>
+                                </div>
+                                <div className="cs_hero_right">
+                                    <div className="cs_hero_thumb">
+                                    <Image
+                                    src={hero2}
+                                    alt="Thumb"
+                                    height={1072}
+                                    width={1140}                          
+                                    ></Image>
+                                    </div>
+                                    <div className="cs_animated_text position-absolute">
+                                    <Image
+                                    src={animated_text}
+                                    alt="animated_text"
+                                    height={300}
+                                    width={300}
+                                    ></Image>
+                                    <span className="cs_text_light cs_accent_bg"></span>
+                                    </div>
+                                </div>
+                                </div>
                             </div>
-                        </div>
-                        <div className="cs_hero_right">
-                            <div className="cs_hero_thumb">
-                            <Image
-                            src={hero2}
-                            alt="Thumb"
-                            height={1072}
-                            width={1140}                          
-                            ></Image>
+                            <div className="slick_slide_in">
+                                <div className="cs_hero cs_style_2 position-relative">
+                                <div className="cs_hero_left">
+                                    <div className="cs_hero_text">
+                                    <h4 className="cs_hero_title_mini cs_fs_28">Up to 20% Off</h4>
+                                    <h1 className="cs_hero_title cs_fs_67 cs_bold">Elegant woman collection in summer</h1>
+                                    <a href="shop.html" className="cs_btn cs_style_1 cs_fs_16 cs_medium">See Collection</a>
+                                    </div>
+                                </div>
+                                <div className="cs_hero_right">
+                                    <div className="cs_hero_thumb">
+                                    <Image
+                                    src={hero2}
+                                    alt="hero5"
+                                    height={1072}
+                                    width={1140}
+                                    ></Image>
+                                    </div>
+                                    <div className="cs_animated_text position-absolute">
+                                    <Image
+                                    src={animated_text}
+                                    alt="Animated Text"
+                                    height={300}
+                                    width={300}
+                                    ></Image>
+                                    <span className="cs_text_light cs_accent_bg"></span>
+                                    </div>
+                                </div>
+                                </div>
                             </div>
-                            <div className="cs_animated_text position-absolute">
-                            <Image
-                            src={animated_text}
-                            alt="animated_text"
-                            height={300}
-                            width={300}
-                            ></Image>
-                            <span className="cs_text_light cs_accent_bg"></span>
+                            <div className="slick_slide_in">
+                                <div className="cs_hero cs_style_2 position-relative">
+                                <div className="cs_hero_left">
+                                    <div className="cs_hero_text">
+                                    <h4 className="cs_hero_title_mini cs_fs_28">Up to 10% Off</h4>
+                                    <h1 className="cs_hero_title cs_fs_67 cs_bold">Children collection in winter season</h1>
+                                    <a href="shop.html" className="cs_btn cs_style_1 cs_fs_16 cs_medium">See Collection</a>
+                                    </div>
+                                </div>
+                                <div className="cs_hero_right">
+                                    <div className="cs_hero_thumb">
+                                    <Image
+                                    src={hero2}
+                                    alt="Thumb"
+                                    height={1072}
+                                    width={1140}                          
+                                    ></Image></div>
+                                    <div className="cs_animated_text position-absolute">
+                                    <Image
+                                    src={animated_text}
+                                    alt="Animated Text"
+                                    height={300}
+                                    width={300}
+                                    ></Image>
+                                    <span className="cs_text_light cs_accent_bg"></span>
+                                    </div>
+                                </div>
+                                </div>
                             </div>
-                        </div>
-                        </div>
-                    </div>
-                    <div className="slick_slide_in">
-                        <div className="cs_hero cs_style_2 position-relative">
-                        <div className="cs_hero_left">
-                            <div className="cs_hero_text">
-                            <h4 className="cs_hero_title_mini cs_fs_28">Up to 20% Off</h4>
-                            <h1 className="cs_hero_title cs_fs_67 cs_bold">Elegant woman collection in summer</h1>
-                            <a href="shop.html" className="cs_btn cs_style_1 cs_fs_16 cs_medium">See Collection</a>
                             </div>
-                        </div>
-                        <div className="cs_hero_right">
-                            <div className="cs_hero_thumb">
-                            <Image
-                            src={hero2}
-                            alt="hero5"
-                            height={1072}
-                            width={1140}
-                            ></Image>
-                            </div>
-                            <div className="cs_animated_text position-absolute">
-                            <Image
-                            src={animated_text}
-                            alt="Animated Text"
-                            height={300}
-                            width={300}
-                            ></Image>
-                            <span className="cs_text_light cs_accent_bg"></span>
-                            </div>
-                        </div>
-                        </div>
-                    </div>
-                    <div className="slick_slide_in">
-                        <div className="cs_hero cs_style_2 position-relative">
-                        <div className="cs_hero_left">
-                            <div className="cs_hero_text">
-                            <h4 className="cs_hero_title_mini cs_fs_28">Up to 10% Off</h4>
-                            <h1 className="cs_hero_title cs_fs_67 cs_bold">Children collection in winter season</h1>
-                            <a href="shop.html" className="cs_btn cs_style_1 cs_fs_16 cs_medium">See Collection</a>
-                            </div>
-                        </div>
-                        <div className="cs_hero_right">
-                            <div className="cs_hero_thumb">
-                            <Image
-                            src={hero2}
-                            alt="Thumb"
-                            height={1072}
-                            width={1140}                          
-                            ></Image></div>
-                            <div className="cs_animated_text position-absolute">
-                            <Image
-                            src={animated_text}
-                            alt="Animated Text"
-                            height={300}
-                            width={300}
-                            ></Image>
-                            <span className="cs_text_light cs_accent_bg"></span>
-                            </div>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
 
-                </div>
-                </div>
+                        </div>
+                        </div>
+
+                    </Slider>
+                
             </section>
             <div className="cs_slides_numbers">
                     <span className="active">01</span><span className="cs_slide_seperator"> / </span><span className="cs_total_slide"></span>
